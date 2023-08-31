7d3616a3c84544df7981dc5a7ac12acd2c718b65
Anthony Medina <anthony.john.medina@gmail.com>
2023-08-30 20:30:48 -0400

    lottie animations and state management added to playQuiz component

--- src/features/PlayQuiz.tsx
+++ src/features/PlayQuiz.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { QuizItem } from '../types/quiz-types'
 import {
   Flex,
@@ -8,6 +8,11 @@ import {
   Radio,
   Text
 } from '@chakra-ui/react'
+import Lottie from 'lottie-react'
+import correctAnim from '../assets/lottie/correct.json'
+import incorrectAnim from '../assets/lottie/incorrect.json'
+
+// 1. Passing in Quiz which is expected to be an Array of QuizItems. 2. Setting the CurrentQuizItemIndex to 0, which is the first question returned in the response array. 3. Setting the current QuizItem - which is set to whatever the index is at the time. 4. Setting Available Answers to state by including the correct answer and the "spread" incorrect answers into an array of strings.
 
 export const PlayQuiz = (p: { quiz: QuizItem[] }) => {
   const [currentQuizItemIndex, setCurrentQuizItemIndex] = useState<number>(0)
@@ -16,6 +21,10 @@ export const PlayQuiz = (p: { quiz: QuizItem[] }) => {
     currentQuizItem.correct_answer,
     ...currentQuizItem.incorrect_answers
   ]
+  const [answer, setAnswer] = useState<string>()
+  const [questionStatus, setQuestionStatus] = useState<
+    'valid' | 'invalid' | 'unanswered'
+  >('unanswered')
 
   const radioList = availableAnswers.map((availableAnswer: string) => {
     return (
@@ -24,6 +33,20 @@ export const PlayQuiz = (p: { quiz: QuizItem[] }) => {
       </Radio>
     )
   })
+
+  useEffect(() => {
+    if (answer) {
+      if (isValidAnswer(answer)) {
+        setQuestionStatus('valid')
+      } else {
+        setQuestionStatus('invalid')
+      }
+    }
+  }, [answer])
+
+  const isValidAnswer = (answer: string): boolean => {
+    return answer === currentQuizItem.correct_answer
+  }
 
   return (
     <>
@@ -35,14 +58,24 @@ export const PlayQuiz = (p: { quiz: QuizItem[] }) => {
           dangerouslySetInnerHTML={{ __html: currentQuizItem.question }}
         />
 
-        <RadioGroup
-          value={''}
-          onChange={() => {
-            setCurrentQuizItemIndex(currentQuizItemIndex + 1)
-          }}
-        >
+        <RadioGroup value={answer} onChange={setAnswer}>
           <SimpleGrid>{radioList}</SimpleGrid>
         </RadioGroup>
+        <Lottie
+          loop={false}
+          style={{ marginTop: 100, height: 150 }}
+          animationData={
+            questionStatus === 'unanswered'
+              ? null
+              : questionStatus === 'valid'
+              ? correctAnim
+              : incorrectAnim
+          }
+          onComplete={() => {
+            setQuestionStatus('unanswered')
+            setCurrentQuizItemIndex(currentQuizItemIndex + 1)
+          }}
+        />
       </Flex>
     </>
   )
